d1dc1520c4c51bcbffe9beba370933af89434cb7
gvirus21 <gvirus21yt@gmail.com>
2022-09-02 10:52:35 +0530

    complted basic redux practice

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useDispatch} from "react-redux"
+import {login, logout} from "../features/user"
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+        <button onClick={() => dispatch(login({name: "Gourav", age: 20, email: 'gourav@gmail.com'}))}>Login</button>
+        <button onClick={() => dispatch(logout())}>logout</button>
+    </div>
+  )
+}
+
+export default Login
